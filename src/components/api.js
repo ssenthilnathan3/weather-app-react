675a944c4b03dc90c860d619ce999071c550ee7a
Senthilnathan <ssenthil1490@outlook.com>
2022-09-07 21:51:18 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -3,7 +3,7 @@ export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '98cfd60d97msh7fcbaabafca4c77p1b9ea7jsn073275bbf94f',
+    'X-RapidAPI-Key': 'your_api_key',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
   }
 };
